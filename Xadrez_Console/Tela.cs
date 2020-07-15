1160df4681e7c0404e76df8e3851ccfba97465d9
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-15 15:35:29 -0300

    Melhorando a forma de impressão do tabuleiro, com diferenciação de cores.

--- Xadrez_Console/Tela.cs
+++ Xadrez_Console/Tela.cs
@@ -11,15 +11,36 @@ namespace Xadrez_Console
         {
             for (int linhas = 0; linhas < tabuleiro.Linhas; linhas++)
             {
+                Console.Write(8 - linhas + " ");
                 for (int colunas = 0; colunas < tabuleiro.Colunas; colunas++)
                 {
                     if (tabuleiro.Peca(linhas, colunas) == null)
                     {
                         Console.Write("- ");
                     }
-                    Console.Write(tabuleiro.Peca(linhas, colunas) + " ");
+                    else
+                    {
+                        ImprimirPeca(tabuleiro.Peca(linhas, colunas));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
